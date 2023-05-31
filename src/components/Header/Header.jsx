22d4dbde0484b54d641293ccde7ac8e6850908f3
wtfranco22 <francofranco_96@hotmail.com>
2023-05-31 14:33:37 -0300

    terminado la primera parte de agregado de estilos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import SearchBar from './SearchBar';
+import SearchBar from './../SearchBar/SearchBar';
 import styles from './Header.module.css';
 export default function Header() {
     return (
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ export default function SearchBar({onSearch}) {
    return (
       <div>
          <input type='search' />
-         <button onClick={onSearch}>Search</button>
+         <button onClick={onSearch}>Add</button>
       </div>
    );
 }
