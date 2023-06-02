c93415b840cfe6027af957a0f4c66fa0bc0abc9f
wtfranco22 <francofranco_96@hotmail.com>
2023-06-02 13:17:37 -0300

    vista de detalles del personaje, con estilos

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,16 +1,19 @@
-export default function Detail({character}) {
+import styles from './Detail.module.css';
+export default function Detail({ character }) {
     return (
-        <>
-            <img src={character.image} alt={character.name} />
-            <div >
-                <h3 >{character.name}</h3>
-                <p >
+        <div className={styles.container}>
+            <div className={styles.img}>
+                <img src={character.image} alt={character.name} />
+            </div>
+            <div className={styles.description}>
+                <h2>{character.name}</h2>
+                <h3>
                     {'Status: ' + character.status} <br />
                     {'Species: ' + character.species} <br />
                     {'Gender: ' + character.gender} <br />
                     {'Origin: ' + character.origin?.name} <br />
-                </p>
+                </h3>
             </div>
-        </>
+        </div>
     )
 }
