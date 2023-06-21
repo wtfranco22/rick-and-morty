d99c06a6d8c6d50dff35183f8610e96a3837bd06
wtfranco22 <francofranco_96@hotmail.com>
2023-06-21 00:11:24 -0300

    arreglo de tipeo

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -7,9 +7,9 @@ const postFavs = (req, res) => {
 }
 
 const deleteFavs = (req, res) => {
-    const { id } = req.body;
-    myFavs = myFavs.filter((character) => character.id === +id);
-    return res.json(200).json(myFavs);
+    const { id } = req.params;
+    myFavs = myFavs.filter((character) => character.id !== +id);
+    return res.status(200).json(myFavs);
 }
 
 module.exports = {
--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,15 +1,10 @@
 const users = require('./../utils/users');
 const login = (req, res) => {
     const { email, password } = req.query;
-    if (!email || !password) {
-        return res.status(404).json({
-            message: 'Email or Password is missing'
-        });
-    }
     if (users.some((user) => user.email === email && user.password === password)) {
-        return res.status(200).json({ success: true });
+        return res.status(200).json({ access: true });
     } else {
-        return res.status(404).json({ success: false });
+        return res.status(404).json({ access: false });
     }
 }
 module.exports = { login };
