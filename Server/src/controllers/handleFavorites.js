d47ce1ab6d447c16a1e8fd268890ed768452004f
wtfranco22 <francofranco_96@hotmail.com>
2023-06-22 10:25:40 -0300

    actualizado el uso del json para almacenar token o favs del usuario

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -1,18 +1,37 @@
-let myFavs = [];
+const users = require('./../utils/users.json');
+const fs = require('fs');
 
 const postFavs = (req, res) => {
+    let indexUser = req.indexUser;
     let { character } = req.body;
-    myFavs.push(character);
-    return res.status(200).json(myFavs);
+    users[indexUser].favs.push(character);
+    fs.writeFile(__dirname + '/../utils/users.json', JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error al guardar el fav, ' + err.message });
+        }
+        return res.status(200).json(users[indexUser].favs);
+    });
 }
 
 const deleteFavs = (req, res) => {
+    let indexUser = req.indexUser;
     const { id } = req.params;
-    myFavs = myFavs.filter((character) => character.id !== +id);
-    return res.status(200).json(myFavs);
+    users[indexUser].favs = users[indexUser].favs.filter((character) => character.id !== +id);
+    fs.writeFile(__dirname + '/../utils/users.json', JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error al eliminar el fav, ' + err.message });
+        }
+        return res.status(200).json(users[indexUser].favs);
+    });
 }
 
+const getFavs = (req, res) => {
+    let indexUser = req.indexUser;
+    if (users[indexUser].favs) return res.status(200).json({ favs: users[indexUser].favs });
+    return res.status(500).json({ error: 'Error al recargar los favs, ' + err.message });
+}
 module.exports = {
     postFavs,
-    deleteFavs
+    deleteFavs,
+    getFavs
 }
--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,8 +1,18 @@
-const users = require('./../utils/users');
+const users = require('../utils/users.json');
+const fs = require('fs');
 const login = (req, res) => {
     const { email, password } = req.query;
-    if (users.some((user) => user.email === email && user.password === password)) {
-        return res.status(200).json({ access: true });
+    const userIndex = users.findIndex((user) => user.email === email && user.password === password);
+    if (userIndex !== -1) {
+        const token = Math.random().toString(36).substring(2);
+        users[userIndex].token = `Bearer ${token}`;
+        let favs = users[userIndex].favs;
+        fs.writeFile(__dirname + '/../utils/users.json', JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error al guardar el token, ' + err.message });
+            }
+            return res.status(200).json({ access: true, token: token, favs: favs });
+        });
     } else {
         return res.status(404).json({ access: false });
     }
