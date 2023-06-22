924ad7d42b54a9279ed74eaa135829f3e6ea8c7c
wtfranco22 <francofranco_96@hotmail.com>
2023-06-22 10:24:16 -0300

    ruta para obtener todos los favs al recargar la pagina en front

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,12 +1,13 @@
 const { login } = require('./../controllers/login');
 const { getCharById } = require('./../controllers/getCharById');
-const { postFavs, deleteFavs } = require('./../controllers/handleFavorites');
+const { postFavs, deleteFavs, getFavs } = require('./../controllers/handleFavorites');
 
 const router = require('express').Router();
 
 router.get('/character/:id', getCharById);
 router.get('/login', login);
 router.post('/fav', postFavs);
+router.post('/get-favs',getFavs);
 router.delete('/fav/:id', deleteFavs);
 
 module.exports = { router };
